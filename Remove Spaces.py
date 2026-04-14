bfc35830e8d8bca24bf57c28fcbb8096a06b5103
Amit Kumar <amitkumarmjl22@gmail.com>
2026-04-14 16:16:29 +0530

    Day 103
    Implement removeSpaces method to remove spaces from a string.

---
+++ Remove Spaces.py
@@ -0,0 +1,11 @@
+class Solution:
+    def removeSpaces(self, s):
+        # code here
+        # res = []
+        # for a in s:
+        #     if a != ' ':
+        #         res.append(a)
+        # return ''.join(res)
+        
+        # for one liner solution
+        return s.replace(' ', '')
