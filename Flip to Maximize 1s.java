6a220f6f5ebb1c1c5f67f97c5e153a74e0c9e372
Amit Kumar <amitkumarmjl22@gmail.com>
2026-04-18 12:00:32 +0530

    Day 107

---
+++ Flip to Maximize 1s.java
@@ -0,0 +1,19 @@
+class Solution {
+    int maxOnes(int[] arr) {
+        // code here
+        int t_ones = 0;
+        for (int a : arr){
+            t_ones += a;
+        }
+        int pre = 0;
+        int min_pre = 0;
+        int max_gain = 0;
+        for (int a : arr){
+            int val = (a == 0) ? 1 : -1;
+            pre+=val;
+            max_gain = Math.max(max_gain, pre-min_pre);
+            min_pre = Math.min(min_pre, pre);
+        }
+        return t_ones + max_gain;
+    }
+}
