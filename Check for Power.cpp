9f28f86f80746a5d4cda16ab23345cb69710039b
Amit Kumar <amitkumarmjl22@gmail.com>
2026-04-19 20:02:53 +0530

    Day 108

---
+++ Check for Power.cpp
@@ -0,0 +1,17 @@
+class Solution {
+  public:
+    bool isPower(int x, int y) {
+        // code here
+        if (y == 1){
+            return true;
+        }
+        if (x == 1){
+            return false;
+        }
+        while (y % x == 0){
+            y = y / x;
+        }
+        return y == 1;
+    }
+};};
+
